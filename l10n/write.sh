0ab33f31243d6e90fbab8bbc5f02d2173bca7d2f
Julien Veyssier <eneiluj@posteo.net>
2017-11-28 10:08:05 +0100

    rename translation files correctly

---
+++ l10n/write.sh
@@ -0,0 +1,15 @@
+perl l10n.pl write phonetrack
+rename 's/fr_FR/fr/g' *.js*
+rename 's/es_ES/es/g' *.js*
+rename 's/de_DE/de/g' *.js*
+rename 's/ja_JP/ja/g' *.js*
+rename 's/ru_RU/ru/g' *.js*
+rename 's/nl_NL/nl/g' *.js*
+rename 's/it_IT/it/g' *.js*
+rename 's/da_DK/da/g' *.js*
+rename 's/sv_SE/sv/g' *.js*
+rename 's/tr_TR/tr/g' *.js*
+rename 's/ko_KR/ko/g' *.js*
+rename 's/ca_ES/ca/g' *.js*
+rename 's/ro_RO/ro/g' *.js*
+rename 's/no_NO/no/g' *.js*
