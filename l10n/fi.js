d0840ae53250ac5effa2ef1dc8a3117ca0c02781
Julien Veyssier <eneiluj@posteo.net>
2017-12-07 20:42:53 +0100

    gen trans from crowdin

---
+++ l10n/fi.js
@@ -0,0 +1,9 @@
+OC.L10N.register(
+    "phonetrack",
+    {
+    "PhoneTrack" : "PhoneTrack",
+    "left" : "vasen",
+    "right" : "oikea",
+    "Show lines" : "Näytä rivit"
+},
+"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: phonetrack\nX-Crowdin-Language: fi");
