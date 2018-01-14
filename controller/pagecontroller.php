ced6fb33763a0c82b5f2e27b33b9cceaebf02126
Julien Veyssier <eneiluj@posteo.net>
2018-01-14 17:55:45 +0100

    delete devices when deleting session

--- controller/pagecontroller.php
+++ controller/pagecontroller.php
@@ -475,6 +475,12 @@ class PageController extends Controller {
             $req->execute();
             $req->closeCursor();
 
+            $sqldel = 'DELETE FROM *PREFIX*phonetrack_devices ';
+            $sqldel .= 'WHERE sessionid='.$this->db_quote_escape_string($token).';';
+            $req = $this->dbconnection->prepare($sqldel);
+            $req->execute();
+            $req->closeCursor();
+
             $ok = 1;
         }
         else {
