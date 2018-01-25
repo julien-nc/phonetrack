cbd5b161feb1e28ac318b7c9d870e1b30cc49188
Julien Veyssier <jveyssier@posteo.net>
2018-01-25 14:10:40 +0100

    test phpunit coverage after installing php-xdebug

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -6,7 +6,7 @@
 set -xe
 
 apt-get update -yqq
-apt-get install git sudo -yqq
+apt-get install git sudo php-xdebug -yqq
 
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
