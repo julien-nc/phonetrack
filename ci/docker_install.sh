2a453a89c6ac0c848895efde3034b79ff30ed113
Julien Veyssier <jveyssier@posteo.net>
2018-01-25 18:10:06 +0100

    forgot to install wget in tests...

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -6,7 +6,7 @@
 set -xe
 
 apt-get update -yqq
-apt-get install git sudo php-xdebug php7.1-sqlite3 unzip curl -yqq
+apt-get install git sudo php-xdebug php7.1-sqlite3 unzip curl wget -yqq
 
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
