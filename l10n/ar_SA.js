df8fcd640b72d4497cac1dff87994e2eb79b3ac7
Julien Veyssier <eneiluj@posteo.net>
2018-02-20 11:34:51 +0100

    gen new trans files, read new strings

---
+++ l10n/ar_SA.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "phonetrack",
+    {
+    "PhoneTrack" : "تتبع الهاتف",
+    "left" : "يسار",
+    "right" : "يمين",
+    "Show lines" : "إظهار الخطوط",
+    "Hide lines" : "إخفاء الخطوط",
+    "Activate automatic zoom" : "تنشيط التكبير التلقائي",
+    "Disable automatic zoom" : "تعطيل التكبير التلقائي",
+    "Show last point tooltip" : "إظهار تلميح آخر نقطة",
+    "Hide last point tooltip" : "إخفاء تلميح آخر نقطة",
+    "Zoom on all devices" : "تكبير على جميع الأجهزة",
+    "Click on the map to move the point, press ESC to cancel" : "انقر على الخريطة لتحريك النقطة، اضغط المفتاح ESC إلغاء الأمر",
+    "Server name or server url should not be empty" : "اسم او عنوان المخدّم يجب أن لا يكون فارغاً",
+    "A server with this name already exists" : "يوجد مخدّم بنفس الاسم بالفعل",
+    "Delete" : "حذف",
+    "Tile server \"{ts}\" has been added" : "تم إضافة مخدّم \"{ts}\""
+},
+"nplurals=6; plural=(n==0 ? 0 : n==1 ? 1 : n==2 ? 2 : n%100>=3 && n%100<=10 ? 3 : n%100>=11 && n%100<=99 ? 4 : 5);\nX-Generator: crowdin.com\nX-Crowdin-Project: phonetrack\nX-Crowdin-Language: ar");
