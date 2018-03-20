b0a1a1fe6df2b2154e72adc152e81bbf19d0f2b7
Julien Veyssier <eneiluj@posteo.net>
2018-03-20 14:49:58 +0100

    refs #83 adjust loading notification size

--- js/phonetrack.js
+++ js/phonetrack.js
@@ -493,7 +493,7 @@
             position: 'topright',
             //minSize: [70, 70],
             //maxSize: [70, 70],
-            size: [55, 60]
+            size: [50, 53]
         })
         .setContent(notificationText)
 
