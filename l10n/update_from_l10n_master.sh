4d1e4e89e54ad76d38445095e19a2bb7c317c18d
Julien Veyssier <eneiluj@posteo.net>
2018-03-21 15:24:52 +0100

    update translation memo [skip ci]

--- l10n/update_from_l10n_master.sh
+++ l10n/update_from_l10n_master.sh
@@ -3,10 +3,12 @@ git checkout master
 git pull origin master
 git checkout l10n_master
 git reset --hard HEAD~200
-git pull origin master:l10n_master
 git pull origin l10n_master
 git rebase -i master
 git checkout master
 git merge l10n_master
+cd l10n
+./write
+git commit -a -m "write new translations"
 git push origin master
 git push origin master:l10n_master -f
