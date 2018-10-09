ec5c8610581d692e2bb42dbc11976e3cbcdcfdd8
Julien Veyssier <eneiluj@posteo.net>
2018-10-09 12:49:12 +0200

    write new translations [skip ci]

--- l10n/en_GB.js
+++ l10n/en_GB.js
@@ -1,7 +1,7 @@
 OC.L10N.register(
     "phonetrack",
     {
-    "PhoneTrack" : "PhoneTrack"
-    },
-"nplurals=2; plural=(n > 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: phonetrack\nX-Crowdin-Language: en");
-
+    "PhoneTrack" : "PhoneTrack",
+    "left" : "left"
+},
+"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: phonetrack\nX-Crowdin-Language: en");
