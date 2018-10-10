b90a45ad114e7720a567dedab7ee784ec4e233bf
Valdnet <valdnet@gmail.com>
2018-10-10 19:47:24 +0000

    Grouping settings #1

--- templates/maincontent.php
+++ templates/maincontent.php
@@ -41,7 +41,7 @@ p($_['phonetrack_version']);
                 </button>
             </div>
             <div id="newsessiondiv">
-                <label for="sessionnameinput"><?php p($l->t('Session name'));?></label>
+                <label for="sessionnameinput"><?php p($l->t('Session name')); ?></label>
                 <input type="text" id="sessionnameinput"/>
                 <button id="newsession">
                     <i class="fa fa-check" aria-hidden="true"></i>
@@ -60,129 +60,140 @@ p($_['phonetrack_version']);
         <div style="clear:both"></div>
         <div id="optionscontent" style="display:none;">
         <div id="optioncheckdiv">
+            <div>
+            <hr/>
+            <h2 id="optiontitle" class="sectiontitle"><?php p($l->t('Map settings')); ?></h2>
+            </div>
             <input id="autozoom" type="checkbox"/>
             <label for="autozoom"><i class="fa nc-theming-main-background"> </i>
-            <?php p($l->t('Auto zoom'));?></label>
+            <?php p($l->t('Auto zoom')); ?></label>
             <br/>
             <input id="viewmove" type="checkbox" checked/>
             <label for="viewmove"><i class="fa nc-theming-main-background"> </i>
-            <?php p($l->t('Show lines'));?></label>
+            <?php p($l->t('Show lines')); ?></label>
             <br/>
             <input id="showtime" type="checkbox"/>
             <label for="showtime"><i class="fa nc-theming-main-background"> </i>
-            <?php p($l->t('Show tooltips'));?></label>
+            <?php p($l->t('Show tooltips')); ?></label>
             <br/>
             <label for="updateinterval"><i class="fa fa-sync-alt" aria-hidden="true"></i>
-            <?php p($l->t('Refresh each (sec)'));?></label>
+            <?php p($l->t('Refresh each (sec)')); ?></label>
             <input id="updateinterval" type="number" min="0" max="100000" step="1" value="15"/>
             <div id="countdown"></div>
             <button id="refreshButton"><i class="fa fa-sync-alt" aria-hidden="true"></i> <?php p($l->t('Refresh')); ?></button>
             <br/>
             <div title="<?php p($l->t('An empty value means no limit')); ?>">
             <label for="nbpointsload"><i class="fas fa-ellipsis-v" aria-hidden="true"></i>
-            <?php p($l->t('Max number of points per device to load on refresh'));?></label>
+            <?php p($l->t('Max number of points per device to load on refresh')); ?></label>
             <input id="nbpointsload" type="number" min="1" max="400000000" step="1" value="1000"/> <?php p($l->t('points')); ?>
             </div>
             <div title="<?php p($l->t('Cutting lines only affects map view and stats table')); ?>">
             <label for="cutdistance"><i class="fa fa-cut" aria-hidden="true"></i>
-            <?php p($l->t('Minimum distance to cut between two points'));?></label>
+            <?php p($l->t('Minimum distance to cut between two points')); ?></label>
             <input id="cutdistance" type="number" min="1" max="40000000" step="1" value=""/> <?php p($l->t('meters')); ?>
             </div>
             <div title="<?php p($l->t('Cutting lines only affects map view and stats table')); ?>">
             <label for="cuttime"><i class="fa fa-cut" aria-hidden="true"></i>
-            <?php p($l->t('Minimum time to cut between two points'));?></label>
+            <?php p($l->t('Minimum time to cut between two points')); ?></label>
             <input id="cuttime" type="number" min="1" max="100000000" step="1" value=""/> <?php p($l->t('seconds')); ?>
+            </div>
+            <div>
+            <hr/>
+            <h2 id="optiontitle" class="sectiontitle"><?php p($l->t('Point settings')); ?></h2>
             </div>
             <input id="markerletter" type="checkbox" checked/>
             <label for="markerletter"><i class="fa fa-font" aria-hidden="true"></i>
-            <?php p($l->t('Display first letter of device name on last position'));?></label>
+            <?php p($l->t('Display first letter of device name on last position')); ?></label>
             <br/>
             <input id="linearrow" type="checkbox"/>
             <label for="linearrow"><i class="fa fa-arrow-right" aria-hidden="true"></i>
-            <?php p($l->t('Show direction arrows along lines'));?></label>
+            <?php p($l->t('Show direction arrows along lines')); ?></label>
             <br/>
             <input id="linegradient" type="checkbox"/>
             <label for="linegradient"><i class="fa fa-paint-brush" aria-hidden="true"></i>
-            <?php p($l->t('Draw line with color gradient'));?></label>
+            <?php p($l->t('Draw line with color gradient')); ?></label>
             <br/>
             <input id="tooltipshowaccuracy" type="checkbox" checked/>
             <label for="tooltipshowaccuracy"><i class="far fa-dot-circle" aria-hidden="true"></i>
-            <?php p($l->t('Show accuracy in tooltips'));?> *</label>
+            <?php p($l->t('Show accuracy in tooltips')); ?> *</label>
             <br/>
             <input id="tooltipshowspeed" type="checkbox" checked/>
             <label for="tooltipshowspeed"><i class="fa fa-tachometer-alt" aria-hidden="true"></i>
-            <?php p($l->t('Show speed in tooltips'));?> *</label>
+            <?php p($l->t('Show speed in tooltips')); ?> *</label>
             <br/>
             <input id="tooltipshowbearing" type="checkbox" checked/>
             <label for="tooltipshowbearing"><i class="fa fa-compass" aria-hidden="true"></i>
-            <?php p($l->t('Show bearing in tooltips'));?> *</label>
+            <?php p($l->t('Show bearing in tooltips')); ?> *</label>
             <br/>
             <input id="tooltipshowsatellites" type="checkbox" checked/>
             <label for="tooltipshowsatellites"><i class="fa fa-signal" aria-hidden="true"></i>
-            <?php p($l->t('Show satellites in tooltips'));?> *</label>
+            <?php p($l->t('Show satellites in tooltips')); ?> *</label>
             <br/>
             <input id="tooltipshowbattery" type="checkbox" checked/>
             <label for="tooltipshowbattery"><i class="fa fa-battery-half" aria-hidden="true"></i>
-            <?php p($l->t('Show battery level in tooltips'));?> *</label>
+            <?php p($l->t('Show battery level in tooltips')); ?> *</label>
             <br/>
             <input id="tooltipshowelevation" type="checkbox" checked/>
             <label for="tooltipshowelevation"><i class="fa fa-chart-area" aria-hidden="true"></i>
-            <?php p($l->t('Show elevation in tooltips'));?> *</label>
+            <?php p($l->t('Show elevation in tooltips')); ?> *</label>
             <br/>
             <input id="tooltipshowuseragent" type="checkbox" checked/>
             <label for="tooltipshowuseragent"><i class="fa fa-mobile-alt" aria-hidden="true" style="font-size: 20px"></i>
-            <?php p($l->t('Show user-agent in tooltips'));?> *</label>
+            <?php p($l->t('Show user-agent in tooltips')); ?> *</label>
             <br/>
-
             <input id="dragcheck" type="checkbox" checked/>
             <label for="dragcheck"><i class="far fa-hand-paper" aria-hidden="true"></i>
-            <?php p($l->t('Make points draggable in edition mode'));?></label>
+            <?php p($l->t('Make points draggable in edition mode')); ?></label>
             <br/>
             <input id="acccirclecheck" type="checkbox" checked/>
             <label for="acccirclecheck"><i class="far fa-circle" aria-hidden="true"></i>
-            <?php p($l->t('Show accuracy circle on hover'));?></label>
+            <?php p($l->t('Show accuracy circle on hover')); ?></label>
             <br/>
             <label for="linewidth"><i class="fa fa-pencil-alt" aria-hidden="true"></i>
-            <?php p($l->t('Line width'));?></label>
+            <?php p($l->t('Line width')); ?></label>
             <input id="linewidth" type="range" min="1" max="20" step="1" value="4"/>
             <label id="linewidthlabel">4px</label>
             <br/>
             <label for="pointradius"><i class="fa fa-circle" aria-hidden="true"></i>
-            <?php p($l->t('Point radius'));?></label>
+            <?php p($l->t('Point radius')); ?></label>
             <input id="pointradius" type="range" min="4" max="20" step="1" value="8"/>
             <label id="pointradiuslabel">8px</label>
             <br/>
             <label for="pointlinealpha"><i class="fa fa-eraser" aria-hidden="true"></i>
-            <?php p($l->t('Points and lines opacity'));?></label>
+            <?php p($l->t('Points and lines opacity')); ?></label>
             <input id="pointlinealpha" type="range" min="0.1" max="1" step="0.1" value="0.8"/>
             <label id="pointlinealphalabel">0.8</label>
             <br/>
             <label for="colorthemeselect"><i class="fa fa-paint-brush" aria-hidden="true"></i>
-            <?php p($l->t('Theme'));?>*</label>
+            <?php p($l->t('Theme')); ?>*</label>
             <select id="colorthemeselect">
-                <option value="bright"><?php p($l->t('bright'));?></option>
-                <option value="pastel"><?php p($l->t('pastel'));?></option>
-                <option value="dark"><?php p($l->t('dark'));?></option>
+                <option value="bright"><?php p($l->t('bright')); ?></option>
+                <option value="pastel"><?php p($l->t('pastel')); ?></option>
+                <option value="dark"><?php p($l->t('dark')); ?></option>
             </select>
             <br/>
+            <div>
+            <hr/>
+            <h2 id="optiontitle" class="sectiontitle"><?php p($l->t('File export settings')); ?></h2>
+            </div>
             <label for="autoexportpath"><i class="far fa-save" aria-hidden="true"></i>
-            <?php p($l->t('Auto export path'));?></label>
+            <?php p($l->t('Auto export path')); ?></label>
             <input id="autoexportpath" type="text" value="/PhoneTrack_export"/>
             <br/>
             <input id="exportoneperdev" type="checkbox"/>
             <label for="exportoneperdev"><i class="fas fa-save" aria-hidden="true"></i>
-            <?php p($l->t('Export one file per device'));?></label>
+            <?php p($l->t('Export one file per device')); ?></label>
             <br/>
-            <label>(*) <?php p($l->t('reload page to make changes effective'));?></label>
+            <label>(*) <?php p($l->t('reload page to make changes effective')); ?></label>
         </div>
         </div>
     </div>
     <div id="logmediv">
         <hr/>
         <div id="logmesubdiv">
-            <label for="logmedeviceinput"><?php p($l->t('Device name'));?></label>
+            <label for="logmedeviceinput"><?php p($l->t('Device name')); ?></label>
             <input type="text" id="logmedeviceinput" value="1"/>
-            <label for="logme"><?php p($l->t('Log my position in this session'));?></label>
+            <label for="logme"><?php p($l->t('Log my position in this session')); ?></label>
             <input type="checkbox" id="logme"/>
         </div>
     </div>
