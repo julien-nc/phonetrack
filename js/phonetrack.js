18009d4f3b8da13e2c5df533051bae068dc8911f
Jason Raveling <jason@webunraveling.com>
2019-01-09 00:39:38 +0000

    Fixing formatting of instructions for Owntracks

--- js/phonetrack.js
+++ js/phonetrack.js
@@ -4986,7 +4986,9 @@
         }
         else if (logger === 'owntracks') {
             loggerName = 'Owntracks';
-            content = 'In the Owntracks preferences menu, go to \'Connections\'. Change the connection Mode to \'Private HTTP\', Copy the URL below into the \'Host\' field. Leave settings under \'Identification\' blank as they are not required.';
+            content = t('phonetrack', 'In the Owntracks preferences menu, go to \'Connections\'.') +
+                    ' ' + t('phonetrack', 'Change the connection Mode to \'Private HTTP\', Copy the URL below into the \'Host\' field.') +
+                    ' ' + t('phonetrack', 'Leave settings under \'Identification\' blank as they are not required.');
         }
         else if (logger === 'ulogger') {
             loggerName = 'Ulogger';
