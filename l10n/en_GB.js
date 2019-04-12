4694d3328cc265fce9ef5c3b6058c9f265097e23
Julien Veyssier <eneiluj@posteo.net>
2019-04-12 11:34:49 +0200

    update l10n stuff

--- l10n/en_GB.js
+++
@@ -1,6 +0,0 @@
-OC.L10N.register(
-    "phonetrack",
-    {
-    "PhoneTrack" : "PhoneTrack"
-},
-"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: phonetrack\nX-Crowdin-Language: en-GB");
--- l10n/fi.js
+++
@@ -1,9 +0,0 @@
-OC.L10N.register(
-    "phonetrack",
-    {
-    "PhoneTrack" : "PhoneTrack",
-    "left" : "vasen",
-    "right" : "oikea",
-    "Show lines" : "Näytä rivit"
-},
-"nplurals=2; plural=(n != 1);\nX-Generator: crowdin.com\nX-Crowdin-Project: phonetrack\nX-Crowdin-Language: fi");
