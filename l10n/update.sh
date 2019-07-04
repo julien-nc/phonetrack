2d77f5ee50167cfde10b2f1e4a460f97d4bed677
Julien Veyssier <eneiluj@posteo.net>
2019-07-04 17:38:58 +0200

    update update.sh

--- l10n/update.sh
+++ l10n/update.sh
@@ -4,8 +4,10 @@ git checkout l10n_master
 git reset --hard HEAD~200
 git pull http l10n_master
 rm -rf /tmp/translationfiles ; cp -r ../translationfiles /tmp
+rm -rf /tmp/descriptions ; cp -r ../l10n/descriptions /tmp
 git checkout master
 cp -r /tmp/translationfiles ../
+cp -r /tmp/descriptions ../l10n/
 git commit -a -m "new translations from crowdin"
 
 #git checkout l10n_master
