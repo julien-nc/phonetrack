3da6cafa798603ef7a171624c3302ef471d6362f
Julien Veyssier <eneiluj@posteo.net>
2020-01-01 13:36:15 +0100

    update l10n update script [skip ci]
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- l10n/update.sh
+++ l10n/update.sh
@@ -4,10 +4,10 @@ git checkout l10n_master
 git reset --hard HEAD~200
 git pull origin l10n_master
 rm -rf /tmp/translationfiles ; cp -r ../translationfiles /tmp
-rm -rf /tmp/descriptions ; cp -r ../l10n/descriptions /tmp
+rm -rf /tmp/descriptions ; mkdir /tmp/descriptions ; cp -r ./descriptions/[a-z][a-z]_[A-Z][A-Z] /tmp/descriptions/
 git checkout master
 cp -r /tmp/translationfiles ../
-cp -r /tmp/descriptions ../l10n/
+cp -r /tmp/descriptions ./
 git commit -a -m "new translations from crowdin"
 
 #git checkout l10n_master
