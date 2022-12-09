b0ba0bb26c4634d619dca3fce59f6ea462b0ef09
Julien Veyssier <julien-nc@posteo.net>
2022-12-09 18:41:18 +0100

    gd issue with stable24 in gitlab ci...
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,6 +8,7 @@ set -xe
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -yqq
 apt-get install git sed curl unzip libzip-dev libpq-dev sqlite3 libsqlite3-dev libcurl4-openssl-dev libxml2-dev libpng-dev zip -yqq > /dev/null 2>&1
+apt-get install libgd-dev -yqq
 #apt-get install php-xdebug php-sqlite3 php-curl php-xml php-mbstring php-json php-zip php-gd php-intl -yqq
 #docker-php-ext-install json
 docker-php-ext-install gd
