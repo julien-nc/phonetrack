378c0acabe77f3e5959c1770aaa199fd24024a10
Julien Veyssier <julien-nc@posteo.net>
2025-06-08 14:20:57 +0200

    update composer deps, fix tests
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,27 +1,9 @@
 <?php
 
-/**
- * PhoneTrack
- *
- * This file is licensed under the Affero General Public License version 3 or
- * later. See the COPYING file.
- *
- */
-define('PHPUNIT_RUN', 1);
+require_once __DIR__ . '/../../../tests/bootstrap.php';
 
-// ugly hack to fix issues with template code using static code
-$_SERVER['REQUEST_URI'] = '/index.php/apps/phonetrack/';
-$_SERVER['SCRIPT_NAME'] = '/index.php';
+use OCA\PhoneTrack\AppInfo\Application;
+use OCP\App\IAppManager;
 
-require_once __DIR__ . '/../../../lib/base.php';
-
-if (version_compare(implode('.', \OCP\Util::getVersion()), '8.2', '>=')) {
-	\OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
-	\OC_App::loadApp('phonetrack');
-}
-
-//if(!class_exists('PHPUnit_Framework_TestCase')) {
-//	require_once('PHPUnit/Autoload.php');
-//}
-
+\OC::$server->get(IAppManager::class)->loadApp(Application::APP_ID);
 OC_Hook::clear();
