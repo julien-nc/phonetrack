edc605c4fcb6e02a43f21fcae9fa5ab67ca1d38c
Julien Veyssier <julien-nc@posteo.net>
2025-10-27 00:01:52 +0100

    update composer and npm dependencies, use parallel php-cs mode
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 require_once './vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 $config = new Config();
 $config
+	->setParallelConfig(ParallelConfigFactory::detect())
 	->getFinder()
 	->notPath('build')
 	->notPath('l10n')
