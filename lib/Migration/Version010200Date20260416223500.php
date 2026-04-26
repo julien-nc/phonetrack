bce0268ab29e31b27f3cac77c8480d3ef6ab9210
Julien Veyssier <julien-nc@posteo.net>
2026-04-26 12:49:37 +0200

    run cs:fix
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- lib/Migration/Version010200Date20260416223500.php
+++ lib/Migration/Version010200Date20260416223500.php
@@ -6,17 +6,17 @@ namespace OCA\PhoneTrack\Migration;
 
 use Closure;
 use Doctrine\DBAL\Types\Type;
+use OCP\DB\Types;
 use OCP\Migration\IOutput;
 use OCP\Migration\SimpleMigrationStep;
-use OCP\DB\Types;
 
 class Version010200Date20260416223500 extends SimpleMigrationStep {
 
-    /**
-     * Add the missing database columns for the Device and Session models
-     * that weren't added when upgrading from <1 to 1.0
-     * If they are already there, make sure they have the right type and make them unsigned
-     */
+	/**
+	 * Add the missing database columns for the Device and Session models
+	 * that weren't added when upgrading from <1 to 1.0
+	 * If they are already there, make sure they have the right type and make them unsigned
+	 */
 	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
 		$schema = $schemaClosure();
 		$schemaChanged = false;
@@ -24,90 +24,90 @@ class Version010200Date20260416223500 extends SimpleMigrationStep {
 		if ($schema->hasTable('phonetrack_devices')) {
 			$table = $schema->getTable('phonetrack_devices');
 
-            if (!$table->hasColumn('line_enabled')) {
-                $table->addColumn('line_enabled', Types::INTEGER, [
-                    'notnull' => true,
+			if (!$table->hasColumn('line_enabled')) {
+				$table->addColumn('line_enabled', Types::INTEGER, [
+					'notnull' => true,
 					'default' => 0,
 					'unsigned' => true,
 				]);
-                $schemaChanged = true;
-            } else {
-                $column = $table->getColumn('line_enabled');
-                if ($column->getType() !== Types::INTEGER || !$column->getUnsigned()) {
-                    $column->setType(Type::getType(Types::INTEGER));
-                    $column->setUnsigned(true);
-                    $schemaChanged = true;
-                }
-            }
+				$schemaChanged = true;
+			} else {
+				$column = $table->getColumn('line_enabled');
+				if ($column->getType() !== Types::INTEGER || !$column->getUnsigned()) {
+					$column->setType(Type::getType(Types::INTEGER));
+					$column->setUnsigned(true);
+					$schemaChanged = true;
+				}
+			}
 
-            if (!$table->hasColumn('auto_zoom')) {
-                $table->addColumn('auto_zoom', Types::INTEGER, [
-                    'notnull' => true,
-                    'default' => 0,
-                    'unsigned' => true,
-                ]);
-                $schemaChanged = true;
-            } else {
-                $column = $table->getColumn('auto_zoom');
-                if ($column->getType() !== Types::INTEGER || !$column->getUnsigned()) {
-                    $column->setType(Type::getType(Types::INTEGER));
-                    $column->setUnsigned(true);
-                    $schemaChanged = true;
-                }
-            }
+			if (!$table->hasColumn('auto_zoom')) {
+				$table->addColumn('auto_zoom', Types::INTEGER, [
+					'notnull' => true,
+					'default' => 0,
+					'unsigned' => true,
+				]);
+				$schemaChanged = true;
+			} else {
+				$column = $table->getColumn('auto_zoom');
+				if ($column->getType() !== Types::INTEGER || !$column->getUnsigned()) {
+					$column->setType(Type::getType(Types::INTEGER));
+					$column->setUnsigned(true);
+					$schemaChanged = true;
+				}
+			}
 
-            if (!$table->hasColumn('color_criteria')) {
-                $table->addColumn('color_criteria', Types::INTEGER, [
-                    'notnull' => true,
-                    'default' => 0,
-                    'unsigned' => true,
-                ]);
-                $schemaChanged = true;
-            } else {
-                $column = $table->getColumn('color_criteria');
-                if ($column->getType() !== Types::INTEGER || !$column->getUnsigned()) {
-                    $column->setType(Type::getType(Types::INTEGER));
-                    $column->setUnsigned(true);
-                    $schemaChanged = true;
-                }
-            }
+			if (!$table->hasColumn('color_criteria')) {
+				$table->addColumn('color_criteria', Types::INTEGER, [
+					'notnull' => true,
+					'default' => 0,
+					'unsigned' => true,
+				]);
+				$schemaChanged = true;
+			} else {
+				$column = $table->getColumn('color_criteria');
+				if ($column->getType() !== Types::INTEGER || !$column->getUnsigned()) {
+					$column->setType(Type::getType(Types::INTEGER));
+					$column->setUnsigned(true);
+					$schemaChanged = true;
+				}
+			}
 
-            if (!$table->hasColumn('enabled')) {
-                $table->addColumn('enabled', Types::INTEGER, [
-                    'notnull' => true,
-                    'default' => 0,
-                    'unsigned' => true,
-                ]);
-                $schemaChanged = true;
-            } else {
-                $column = $table->getColumn('enabled');
-                if ($column->getType() !== Types::INTEGER || !$column->getUnsigned()) {
-                    $column->setType(Type::getType(Types::INTEGER));
-                    $column->setUnsigned(true);
-                    $schemaChanged = true;
-                }
-            }
+			if (!$table->hasColumn('enabled')) {
+				$table->addColumn('enabled', Types::INTEGER, [
+					'notnull' => true,
+					'default' => 0,
+					'unsigned' => true,
+				]);
+				$schemaChanged = true;
+			} else {
+				$column = $table->getColumn('enabled');
+				if ($column->getType() !== Types::INTEGER || !$column->getUnsigned()) {
+					$column->setType(Type::getType(Types::INTEGER));
+					$column->setUnsigned(true);
+					$schemaChanged = true;
+				}
+			}
 		}
 
-        if ($schema->hasTable('phonetrack_sessions')) {
-            $table = $schema->getTable('phonetrack_sessions');
+		if ($schema->hasTable('phonetrack_sessions')) {
+			$table = $schema->getTable('phonetrack_sessions');
 
-            if (!$table->hasColumn('enabled')) {
-                $table->addColumn('enabled', Types::INTEGER, [
-                    'notnull' => true,
-                    'default' => 0,
-                    'unsigned' => true,
-                ]);
-                $schemaChanged = true;
-            } else {
-                $column = $table->getColumn('enabled');
-                if ($column->getType() !== Types::INTEGER || !$column->getUnsigned()) {
-                    $column->setType(Type::getType(Types::INTEGER));
-                    $column->setUnsigned(true);
-                    $schemaChanged = true;
-                }
-            }
-        }
+			if (!$table->hasColumn('enabled')) {
+				$table->addColumn('enabled', Types::INTEGER, [
+					'notnull' => true,
+					'default' => 0,
+					'unsigned' => true,
+				]);
+				$schemaChanged = true;
+			} else {
+				$column = $table->getColumn('enabled');
+				if ($column->getType() !== Types::INTEGER || !$column->getUnsigned()) {
+					$column->setType(Type::getType(Types::INTEGER));
+					$column->setUnsigned(true);
+					$schemaChanged = true;
+				}
+			}
+		}
 
 		return $schemaChanged ? $schema : null;
 	}
